633d65d6ed8ee403b8e2b6c9856fc4ce93e662cf
Lawal Sulaiman <lawal2401gmail.com>
2026-09-07 17:01:28 +0000

    Test infrastructure deployment gate

--- locals.tf
+++ locals.tf
@@ -2,8 +2,9 @@ locals {
   workspace_environment = terraform.workspace == "default" ? "dev" : terraform.workspace
 
   base_labels = {
-    managed_by = "terraform"
-    owner      = "devops-training"
+    deployment_gate_test = "true"
+    managed_by           = "terraform"
+    owner                = "devops-training"
   }
 
   environment_labels = {
